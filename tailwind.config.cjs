92f4e44ad05ff44701f6aa40bc60a854d997ed51
Iniubong Obonguko <iniubongobonguko2018@gmail.com>
2022-10-24 11:20:15 +0100

    tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,25 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{html,js}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  mode: "jit",
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "edvora-purple": "#6A35FF",
+        "edvora-purple-10": "#F0EBFF",
+        "edvora-purple-20": "#ECE6FF",
+        "edvora-purple-70": "#A281FF",
+        "edvora-purple-80": "#8F67FF",
+        "edvora-yellow": "#FFBC00",
+        "edvora-orange": "#F56C22",
+        "edvora-orange-50": "#FCE7DC",
+        "edvora-white-20": "#A0A6B1",
+        "edvora-white-50": "#D3D3D3",
+        "edvora-white-60": "#6C6C6C",
+        "edvora-green": "#39BA62",
+        "edvora-green-50": "#E2FAE9",
+        "edvora-black": "#202020",
+      },
+    },
+  },
+  plugins: [],
 };
